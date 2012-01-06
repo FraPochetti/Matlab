d7a1624a74079b602f2be95cd9152edabab8f68f
Francesco Pochetti <fra.pochetti@gmail.com>
2012-01-06 18:50:31 +0100

    third exercise completed but not working properly..mmmm

--- matlab3.m
+++ matlab3.m
@@ -11,7 +11,9 @@ y = data(:,2);
 
 f = fittype('a.*gaussmf(x, [sig1 c1]) + b.*gaussmf(x, [sig2 c2])')
 %fits curve to data using function specified above and creates a fit object whose features are printed into the framework. 
-fit_data = fit(x,y,f)
+%The 'Startpoint' option lets the user select the initial guess. I
+%COMPLETELY INVENTED IT, TRYING RANDOMLY!!!! not exactly a scientific method
+fit_data = fit(x,y,f, 'Startpoint', [.56,.56,.56,.56,.56,.56])
 
 %hold on and hold off lets the user implement multiplot.
 hold on
