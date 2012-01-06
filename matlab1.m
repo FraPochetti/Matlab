ca6774068bf95a057e58cf682ad952d8c0dc687d
Francesco Pochetti <fra.pochetti@gmail.com>
2012-01-06 13:17:23 +0100

    first exercise completed

--- matlab1.m
+++ matlab1.m
@@ -1,3 +1,4 @@
+%shuts down all figures and clears all variables set in the framework
 close all
 clear all
 
@@ -14,6 +15,9 @@ min=-1;
 len=100;
 x=min+(max-min)*rand(1,len)
 
+%the point '.' is a special matlab operator which means "make the operation
+%element by element, or better x(1)*x(1), x(2)*x(2), ecc"
 y = x.^2;
 
+%plots x versus y using blue (default) empty circles.
 plot(x,y,'o')
